175a0986b8e923e34e3564325cfd2adeca119082
Ryo Ito <ritou.06@gmail.com>
2013-09-12 04:01:18 +0900

    add README and travis config

--- cpanfile
+++ cpanfile
@@ -25,4 +25,5 @@ on 'configure' => sub {
 
 on 'test' => sub {
    requires 'Test::More', '0.98';
+   requires 'Test::MockObject', '0';
 };
--- lib/OIDC/Lite/Demo/Server.pm
+++ lib/OIDC/Lite/Demo/Server.pm
@@ -76,7 +76,23 @@ OpenID Connect Demo Server(OP) using OIDC::Lite
 
 =head1 SYNOPSIS
 
-TBD
+For setup, please run the following command.
+
+    # 0. obtain src and carton setup
+    $ git clone https://github.com/ritou/p5-oidc-lite-demo-server.git
+    $ cd p5-oidc-lite-demo-server
+    $ carton install
+    
+    # 1. test
+    $ carton exec -- perl Build.PL
+    $ carton exec -- ./Build test
+    
+    # 2. Run your demo server
+    $ carton exec -- plackup -r -p 5001
+
+When plack is launched, try to access http://localhost:5001/
+If OIDC::Lite::Demo::Client is installed and run server, 
+you are able to try authorization flow with sample client.
 
 =head1 AUTHOR
 
